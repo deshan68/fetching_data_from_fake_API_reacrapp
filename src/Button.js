b0c5400c8bf8c9f9c69395dafcf5cd915f58fedd
“deshan68” <arundeshan@gmail.com>
2023-01-27 17:35:55 -0800

    second commit

---
+++ src/Button.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Button({ buttonText, reqType, setReqType }) {
+  return (
+    <button
+      className={buttonText === reqType ? "selected" : null}
+      type="button"
+      onClick={() => setReqType(buttonText)}
+    >
+      {buttonText}
+    </button>
+  );
+}
---
+++ src/Row.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Cell from "./Cell";
+
+export default function Row({ item }) {
+  return (
+    <tr>
+      {Object.entries(item).map(([key, value]) => {
+        return <Cell key={key} cellData={JSON.stringify(value)} />;
+      })}
+    </tr>
+  );
+}
